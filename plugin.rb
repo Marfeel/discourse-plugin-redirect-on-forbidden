bf8453669f8a4ff997efa5e3d769d4abfd04e41b
marc <marc.garcia@marfeel.com>
2026-03-10 18:08:39 +0100

    feat: hook into Discourse error handling for 301 redirects on forbidden

--- plugin.rb
+++ plugin.rb
@@ -17,4 +17,63 @@ end
 require_relative "lib/redirect_on_forbidden/engine"
 
 after_initialize do
+  on(:site_setting_changed) do |name, _old, _new|
+    if name == :redirect_on_forbidden_enabled
+      RedirectOnForbidden::RedirectRule.reset_cache!
+    end
+  end
+
+  reloadable_patch do
+    ::ApplicationController.rescue_from(Discourse::InvalidAccess) do |e|
+      if SiteSetting.redirect_on_forbidden_enabled
+        category_id = nil
+        topic_slug = nil
+
+        if params[:topic_id] || (params[:controller] == "topics" && params[:id])
+          topic_id = params[:topic_id] || params[:id]
+          topic = Topic.find_by(id: topic_id)
+          if topic
+            category_id = topic.category_id
+            topic_slug = topic.slug
+          end
+        elsif params[:category_id] || (params[:controller] == "categories")
+          category_id = params[:category_id] || params[:id]
+        elsif params[:category_slug_path_with_id]
+          parts = params[:category_slug_path_with_id].split("/")
+          category_id = parts.last.to_i
+          category_id = nil if category_id == 0
+        end
+
+        if category_id
+          redirect_url = RedirectOnForbidden::UrlResolver.resolve(
+            category_id: category_id,
+            topic_slug: topic_slug,
+          )
+
+          if redirect_url
+            if request.format.html?
+              redirect_to redirect_url, status: 301, allow_other_host: true
+              next
+            elsif request.xhr? || request.format.json?
+              render json: {
+                error_type: "invalid_access",
+                redirect_to: redirect_url,
+                category_id: category_id,
+              }, status: 403
+              next
+            end
+          end
+        end
+      end
+
+      rescue_discourse_actions(
+        :invalid_access,
+        403,
+        include_ember: true,
+        custom_message: e.custom_message,
+        custom_message_params: e.custom_message_params,
+        group: e.group,
+      )
+    end
+  end
 end
